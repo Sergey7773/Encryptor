cad6da3bfc5d26116a1cdeb8b88ddcfefd5c83fb
Sergey7773 <sergey235711@gmail.com>
2016-05-23 09:22:11 +0300

    Added file name format generation and random key generation.

--- encryptor/src/main/java/encryptor/encryptor/AlgorithmWrapper.java
+++ encryptor/src/main/java/encryptor/encryptor/AlgorithmWrapper.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Random;
 
 public class AlgorithmWrapper {
 	
@@ -14,20 +15,34 @@ public class AlgorithmWrapper {
 		m_encryptionAlgorithm = algorithm;
 	}
 	
+	private String appedEncryptedToFilename(File f) {
+		return f.getName()+".encrypted";
+	}
+	
+	private String appedDecryptedToFilename(File f) {
+		String $ = f.getName();
+		$=$.replace(".encrypted", "");
+		int lastDot = $.lastIndexOf('.');
+		if(lastDot==-1) return $+".decrypted";
+		$=$.substring(0, lastDot)+"_decrypted"+$.substring(lastDot, $.length());
+		return $;
+	}
+	
 	public void decrypt(File f,OutputStream userOutputStream, byte key) throws IOException {
-		doAction(f, userOutputStream, new EncryptionApplier(m_encryptionAlgorithm, key));
+		File outputFile = new File(appedDecryptedToFilename(f));
+		doAction(f, outputFile, userOutputStream, new EncryptionApplier(m_encryptionAlgorithm, key));
 	}
 	
 	public void encrypt(File f,OutputStream userOutputStream) throws IOException {
-		byte key =0;
-		//TODO:generate random key
-		doAction(f, userOutputStream, new DecryptionApplier(m_encryptionAlgorithm, key));
+		byte[] key = new byte[1];
+		new Random().nextBytes(key);
+		File outputFile = new File(appedEncryptedToFilename(f));
+		doAction(f, outputFile, userOutputStream, new DecryptionApplier(m_encryptionAlgorithm, key[0]));
 	}
 	
-	private void doAction(File f,OutputStream userOutputStream,Applier<Byte,Byte> function) throws IOException {
+	private void doAction(File f,File outputFile,OutputStream userOutputStream,Applier<Byte,Byte> function) throws IOException {
 		FileInputStream fis = new FileInputStream(f);
-		//TODO: create file with correct name
-		FileOutputStream fos = new FileOutputStream(new File(""));
+		FileOutputStream fos = new FileOutputStream(outputFile);
 		
 		byte plain[] = new byte[1];
 		byte cyphered[] = new byte[1];
