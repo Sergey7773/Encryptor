e4d1e7ed3f69698544833187aad1e54af8d29a7e
Sergey7773 <sergey235711@gmail.com>
2016-06-23 22:27:42 +0300

    Added multiplication encryption alg (default implementation) + basic test.

---
+++ encryptor/src/test/java/encryptor/encryptor/MultiplicationEncryptionAlgorithmTest.java
@@ -0,0 +1,39 @@
+package encryptor.encryptor;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MultiplicationEncryptionAlgorithmTest {
+	private MultiplicationEncryptionAlgorithm $;
+	private byte[] plainText;
+	private byte[] key;
+	private byte[] cypheredText;
+	
+	@Before
+	public void setup() {
+		$ = new MultiplicationEncryptionAlgorithm();
+		plainText = new byte[]{Byte.MIN_VALUE, -75,0,75,Byte.MAX_VALUE};
+		key = new byte[1];
+		new Random(System.currentTimeMillis()).nextBytes(key);
+		for(int i=0;i<plainText.length;i++) {
+			cypheredText[i] = (byte) (key[0]*plainText[i]);
+		}
+	}
+	
+	@Test
+	public void applyingMWOOnEncryptedBytes() {
+		for(int i=0;i<plainText.length;i++)
+			assertEquals(cypheredText[i],$.encrypt(plainText[i], key[0]));
+	}
+	
+	@Test
+	public void correctlyDecrypting() {
+		for(int i=0;i<plainText.length;i++) {
+			assertEquals(plainText[i],$.decrypt(cypheredText[i], key[0]));
+		}
+	}
+}
