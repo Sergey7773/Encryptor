1132ff47717ceefa0b4332cdaf0b0b0907d38af0
Sergey7773 <sergey235711@gmail.com>
2016-08-08 09:41:04 +0300

    Removed logger dependency injection.

--- encryptor/src/main/java/encryptor/encryptor/DefaultEncryptorInjector.java
+++ encryptor/src/main/java/encryptor/encryptor/DefaultEncryptorInjector.java
@@ -11,10 +11,9 @@ public class DefaultEncryptorInjector extends AbstractModule{
 
 	@Override
 	protected void configure() {
-		bind(Clock.class).annotatedWith(Names.named("StopWatchClock")).to(MillisClock.class);
-		bind(Logger.class)
-		.annotatedWith(Names.named("ActionLogger"))
-		.toInstance(new TimeKeepingLogger(Main.class.getName()));
+		bind(Clock.class)
+		.annotatedWith(Names.named("StopWatchClock"))
+		.to(MillisClock.class);
 	}
 
 }
